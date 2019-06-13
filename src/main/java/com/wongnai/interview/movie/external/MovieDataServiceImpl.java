a8510b957e0182c89826b594c9af21049dd05b23
konpat <diw_kru_62@hotmail.com>
2019-06-14 02:16:45 +0700

    Step 1 : (MovieDataServiceImpl) Finished

--- src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
+++ src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
@@ -1,10 +1,16 @@
 package com.wongnai.interview.movie.external;
 
+
+import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestOperations;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
 
 @Component
 public class MovieDataServiceImpl implements MovieDataService {
@@ -24,6 +30,39 @@ public class MovieDataServiceImpl implements MovieDataService {
 		// Please noted that you must only read data remotely and only from given source,
 		// do not download and use local file or put the file anywhere else.
 
-		return null;
+
+		MoviesResponse response = null;
+		objectMapper = new ObjectMapper();
+		String json = "";
+		try{
+			json = readJsonFromUrl(MOVIE_DATA_URL);
+			response = objectMapper.readValue(json, MoviesResponse.class);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return response;
 	}
+
+	//Get function readAll & readJsonFromUrl from stackoverflow
+	//Source: "https://stackoverflow.com/questions/4308554/simplest-way-to-read-json-from-a-url-in-java"
+	private static String readAll(Reader rd) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		int cp;
+		while ((cp = rd.read()) != -1) {
+			sb.append((char) cp);
+		}
+		return sb.toString();
+	}
+
+	public static String readJsonFromUrl(String url) throws IOException, JSONException {
+		InputStream is = new URL(url).openStream();
+		try {
+			BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+			String jsonText = readAll(rd);
+			return jsonText;
+		} finally {
+			is.close();
+		}
+	}
+
 }
