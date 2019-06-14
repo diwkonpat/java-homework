23f28a24852f1cd2761403c28e329b68917e610e
konpat <diw_kru_62@hotmail.com>
2019-06-14 11:10:15 +0700

    Step 2 : (Search Glorious word) Finished

--- src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
+++ src/main/java/com/wongnai/interview/movie/search/SimpleMovieSearchService.java
@@ -1,7 +1,11 @@
 package com.wongnai.interview.movie.search;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.wongnai.interview.movie.external.MovieData;
+import com.wongnai.interview.movie.external.MovieDataServiceImpl;
+import com.wongnai.interview.movie.external.MoviesResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +23,34 @@ public class SimpleMovieSearchService implements MovieSearchService {
 		//TODO: Step 2 => Implement this method by using data from MovieDataService
 		// All test in SimpleMovieSearchServiceIntegrationTest must pass.
 		// Please do not change @Component annotation on this class
-		return null;
+
+		List<Movie> result = new ArrayList<>();
+		MoviesResponse response = movieDataService.fetchAll();
+
+		if(!queryText.isEmpty()){
+			for (MovieData i : response){
+				if(iscontain(i.getTitle(),queryText)){
+					Movie movie = new Movie(i.getTitle());
+					List<String> actors = new ArrayList<>(i.getCast());
+					movie.setActors(actors);
+					result.add(movie);
+				}
+			}
+		}
+		//for (Movie i : result) System.out.println(i.getName());
+		//System.out.println(result.size());
+		return result;
+	}
+
+	public boolean iscontain (String sentence, String queryText){
+		boolean check = false ;
+		String[] word = sentence.split(" ");
+		for(String w :word){
+			if((w.toLowerCase().equals(queryText.toLowerCase())) ){
+				check = true ;
+				break;
+			}
+		}
+		return check;
 	}
 }
